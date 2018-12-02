6f5cd87c0c8db23dc8bb949ef8e95a062555ba3c
Manwarul Alim <likhon2093@gmail.com>
2018-12-02 17:35:12 +0600

    Used new image to test turret shooter object

---
+++ Assets/Scripts/PlayOnStateChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnStateChange : MonoBehaviour {
+
+	public AudioClip appearClip;
+	public AudioClip disAppearClip;
+
+	private AudioSource source;
+
+	void Awake() {
+		source = GetComponent<AudioSource>();
+		source.enabled = false;
+	}
+	
+	public void Initialize(){
+		source.enabled = true;
+	}
+
+	public void PlayOnAppear(){
+		source.clip = appearClip;
+		source.Play();
+	}
+
+	public void PlayOnDisAppear(){
+		source.clip = disAppearClip;
+		source.Play();
+	}
+
+}
